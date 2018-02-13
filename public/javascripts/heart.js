23c6c4c152d32c5c8b50f7c44b167a7287f07489
juko <sakikaorucha027@gmail.com>
2018-02-13 22:47:12 +0900

    add image-filter

---
+++ public/javascripts/heart.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(window).load(function() {
+
+    $('img.filter1').simpleFilter({
+	     filter : 'fade',
+	     lightleak : 'lightleak01',
+	     shadow : 'drama'
+	  });
+
+  });
+
+});
