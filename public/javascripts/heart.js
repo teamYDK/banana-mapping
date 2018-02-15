6f957cb1f480db33305d914ce6e13cc758ba9b8a
juko <sakikaorucha027@gmail.com>
2018-02-15 21:34:25 +0900

    add image-filter

--- public/javascripts/heart.js
+++ public/javascripts/heart.js
@@ -1,5 +1,7 @@
 $(function() {
   $(window).load(function() {
+    var image = document.getElementById('background');
+    image.crossOrigin = 'anonymous';
 
     $('img.filter1').simpleFilter({
 	     filter : 'grayscale',
