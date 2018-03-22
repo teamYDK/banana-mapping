3b9644bb2bf08be064f426a2fcb2c232b034a396
juko <sakikaorucha027@gmail.com>
2018-03-22 10:53:21 +0900

    change firebase

--- routes/index.js
+++ routes/index.js
@@ -61,12 +61,29 @@ router.get('/tagged', function(req, res, next) {//文字の表示
 
 // タイムライン
 router.get('/dashbord', function(req, res, next) {//文字の表示
+
   var query = firebase.database().ref('messages').orderByKey();
   query.once('value').then(function(snapshot) {
-    console.log(snapshot.exportVal());
     var messages = [];
     snapshot.forEach(function(childSnapshot) {
       messages.push(childSnapshot.val());
+      const order = [
+          {key: "time", reverse: true},
+      ];
+      // ソート関数（デフォルトで昇順）
+      function sort_by(list) {
+          return (a, b) => {
+              for (let i=0; i<list.length; i++) {
+                  const order_by = list[i].reverse ? 1 : -1;
+                  if (a[list[i].key] < b[list[i].key]) return order_by;
+                  if (a[list[i].key] > b[list[i].key]) return order_by * -1;
+              }
+              return 0;
+          };
+      };
+      // ソート
+      messages.sort(sort_by(order));
+      //console.log(messages);
     });
     res.render('timeline', { title: 'My Mapping', messages: messages });
   });
@@ -181,51 +198,97 @@ router.post('/upload', function(req, res) {//入力データを読み込む
         return dms[0] + ( dms[1] * 60 + dms[2] ) / 3600
       }
 
-/* 写真からの緯度経度のぬきだし */
-      new ExifImage({ image : req.file.path }, function (error, image) {
-        if (error) {
-          console.log('Error: '+error.message);
-        } else {
-          var lat = convert((image['gps']['GPSLatitude']));//スコープの範囲気をつける
-          var lon = convert((image['gps']['GPSLongitude']));//スコープの範囲気をつける
-          console.log(lat, lon);
+      /* 写真からの緯度経度のぬきだし */
+            new ExifImage({ image : req.file.path }, function (error, image) {
+              if (error) {
+                res.send('Error: '+error.message);
+                console.log('Error: '+error.message);
+              } else {
+                var lat = convert((image['gps']['GPSLatitude']));//スコープの範囲気をつける
+                var lon = convert((image['gps']['GPSLongitude']));//スコープの範囲気をつける
+                var latlng_geocode = [lat,lon];
+                console.log(lat, lon);
 
-          var firebaseRef =firebase.database().ref();//追加
-          var messagesRef = firebaseRef.child('messages');// データベースの参照の取得
+                // Geocode an address.
+                googleMapsClient.reverseGeocode({
+                  latlng: latlng_geocode
+                }, function(err, response) {
+                  if (!err) {
+                    var location_id = response.json.results[0].place_id;
+                    googleMapsClient.place({
+                      language: 'ja',
+                      placeid: location_id
+                    }, function(err2, response2) {
+                      if(!err2) {
+                        var location_addr = response2.json.result.adr_address;
+                        var location_name = response2.json.result.name;
+                        console.log('地名：' + location_name);
 
-          var tagRef = firebaseRef.child('tags');
+                        googleMapsClient.placesNearby({
+                          language: 'ja',
+                          location : latlng_geocode,
+                          rankby: 'distance',
+                          type : 'point_of_interest'
+                        }, function(err3, response3) {
+                          if (!err3) {
+                            var place_name = response3.json.results[0].name;
+                            console.log('場所：' + response3.json.results[0].name);
 
-          cloudinary.uploader.upload(req.file.path, function(result) {
-            var imagePath = result.secure_url;
-            console.log(result)
-            messagesRef.push({ // ...　囲んでる部分の描き方は変わらない 非同期処理
-              username: req.body.username,
-              title:    req.body.title,
-              comment:  req.body.comment,
-              lat:      lat,//リクエストした値でない
-              lon:      lon,//リクエストした値でない
-              file:     imagePath,
-              tag: req.body.tag
-            }).then(function(){
-              res.send("Finish Upload!! " + '<br />' + "Filename: "+ req.file.originalname + " as " + req.file.filename + " Size: " + req.file.size);//画面の表示
+                        var firebaseRef =firebase.database().ref();//追加
+                        var messagesRef = firebaseRef.child('messages');// データベースの参照の取得
+                        var firetagRef = firebase.database().ref('tag');
+                        var tagRef = messagesRef.child('tags');
+                        cloudinary.uploader.upload(req.file.path, function(result) {
+                          var imagePath = result.secure_url;
+                          var imageTime = result.created_at;
+                          console.log(result);
+                          messagesRef.push({ // ...　囲んでる部分の描き方は変わらない 非同期処理
+                            username: req.body.username,
+                            title: req.body.title,
+                            comment: req.body.comment,
+                            lat: lat,
+                            lon: lon,
+                            file:     imagePath,
+                            time: imageTime,
+                            address: location_addr,
+                            location: location_name,
+                            place: place_name,
+                            tag: req.body.tag
+                          }).then(function(){
+                            res.send("Finish Upload!! " + '<br />' + "Filename: "+ req.file.originalname + " as " + req.file.filename + " Size: " + req.file.size);//画面の表示
+                            //res.render('home');
+                          });
+                          /*tagRef.child(req.body.tag).push({
+                            username: req.body.username,
+                            title: req.body.title,
+                            comment: req.body.comment,
+                            lat: lat,
+                            lon: lon,
+                            file: imagePath,
+                            tag: req.body.tag
+                          });*/
+                        });　//end of cloudinary_upload
+
+                            } else {
+                              console.log('placesNearby error');
+                            }
+                          }); //end placesNearby
+                      } else {
+                        console.log('ERROR : placedetails()')
+                      }
+                    }); //end placedetails
+                } else {
+                  console.log('Geocode was not successful for the following reason: ' + err);
+                }
+                });
+
+              }
             });
-            tagRef.child(req.body.tag).push({
-              username: req.body.username,
-              title: req.body.title,
-              comment: req.body.comment,
-              lat: lat,
-              lon: lon,
-              file: imagePath,
-              tag: req.body.tag
-            });
-          });
-        }
+
+      /*------------------------------度数秒形式から変換---------------------------*/
+
+          }
+        });
       });
 
-/*------------------------------度数秒形式から変換---------------------------*/
-
-    }
-  });
-});
-
-module.exports=router;
+      module.exports=router;
